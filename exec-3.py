95f99f3f01d831b9d77a2fde0e4caac8b677433d
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:36:33 -0300

    exec-3.py

--- exec-3.py
+++ exec-3.py
@@ -1,4 +1,11 @@
-#Exercicio 3 -  Os pares de instruções abaixo produzem o mesmo resultado? 
+3) Os pares de instruções abaixo produzem o mesmo resultado? Imprima os valores abaixo, veja se algum deles (A, B ou C)
+possuem valores diferentes nas versões 1 e 2, e caso sim, explique num comentário o motivo.
+
+A1 ← (4/2)+(2/4)	e	A2 ← 4/2+2/4
+B1 ← 4/(2+2)/4	e	B2 ← 4/2+2/4
+C1 ← (4+2)*2-4	e	C2 ← 4+2*2-4
+
+
 A =(4/2)+(2/4)
 print(A)
 A=4/2+2/4
