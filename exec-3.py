f08fc9add49aa7b647c2b6819e56973b7028727f
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:10:04 -0300

    exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,13 @@
+#Exercicio 3 -  Os pares de instruções abaixo produzem o mesmo resultado? 
+A =(4/2)+(2/4)
+print(A)
+A=4/2+2/4
+print(A)
+B=4/(2+2)/4
+print(B)
+B=4/2+2/4
+print(B)
+C=(4+2)*2-4
+print(C)
+C=4+2*2-4
+print(C)
